e6b5c90a6dd9a500d7c92e66cfe65269bee3d119
Nicu Savin <savinicu@gmail.com>
2023-08-03 14:00:01 +0200

    dynamic grid and bigger cells

--- packages/website/js/index.js
+++ packages/website/js/index.js
@@ -8,4 +8,19 @@ const renderTable = (grid) => {
 
 fetch('http://localhost:3003/canvas')
   .then((res) => res.json())
-  .then(renderTable);
+  .then((res) => {
+    let table = document.getElementById('gridTable');
+    // each element in the res will be assigned a row
+    res.forEach((row) => {
+      let tr = document.createElement('tr');
+
+      // iterate over the cells
+      row.forEach((cell) => {
+        let td = document.createElement('td');
+        td.style.backgroundColor = cell;
+        tr.appendChild(td);
+      });
+
+      table.appendChild(tr);
+    });
+  });
