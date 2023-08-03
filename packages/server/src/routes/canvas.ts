d213e270e54ed0889c4f696bd563cd976738eb8b
Nicu Savin <savinicu@gmail.com>
2023-08-03 15:47:55 +0200

    added server side color changing

--- packages/server/src/routes/canvas.ts
+++ packages/server/src/routes/canvas.ts
@@ -1,8 +1,10 @@
 import { Response } from 'fets';
 import { router } from '../lib/server.js';
 import { canvas } from '../lib/canvas.js';
-
+import fs from 'fs';
+import { fileURLToPath } from 'url';
 const CANVAS_PATH = '/canvas';
+const UPDATE_PATH = '/update/:row/:col/:color';
 
 router.route({
   path: CANVAS_PATH,
@@ -22,5 +24,41 @@ router.route({
   },
   handler() {
     return Response.json(canvas);
+  }
+});
+
+router.route({
+  path: UPDATE_PATH,
+  method: 'POST',
+  schemas: {
+    body: {
+      type: 'array',
+      items: {
+        type: 'array',
+        items: {
+          type: 'string'
+        }
+      }
+    } as const,
+    responses: {
+      200: {
+        type: 'string'
+      } as const
+    }
+  },
+  handler(request: Request) {
+    const rowIndex = request.params.row;
+    const colIndex = request.params.col;
+    // i had an unknown bug not letting me put the color in the body so this is a small alternative
+    let newColor = request.params.color;
+    newColor = '#' + newColor;
+
+    let newCanvas = canvas;
+    newCanvas[rowIndex][colIndex] = newColor;
+    fs.writeFileSync(
+      fileURLToPath(new URL('../lib/canvas.json', import.meta.url)),
+      JSON.stringify(newCanvas),
+      'utf8'
+    );
   }
 });
--- packages/website/js/index.js
+++ packages/website/js/index.js
@@ -12,20 +12,46 @@ fetch('http://localhost:3003/canvas')
     let table = document.getElementById('gridTable');
     let palette = document.getElementById('palette');
     // each element in the res will be assigned a row
+    let rowIndex = 0;
     res.forEach((row) => {
       let tr = document.createElement('tr');
 
       // iterate over the cells
+      let colIndex = 0;
       row.forEach((cell) => {
         let td = document.createElement('td');
         td.style.backgroundColor = cell;
         tr.appendChild(td);
+        td.id = rowIndex + ' ' + colIndex;
+
+        const [x, y] = td.id.split(' ').map(Number);
+
         td.addEventListener('click', function () {
           // selecting the grid to be colored
-          this.style.backgroundColor = palette.value;
+          let newColor = palette.value;
+          if (this.style.backgroundColor !== newColor) {
+            this.style.backgroundColor = newColor;
+
+            // needed otherwise wont work
+            newColor = newColor.replace('#', '');
+            // i had an unknown bug not letting me put the color in the body so this is a small alternative
+            fetch(`http://localhost:3003/update/${x}/${y}/${newColor}`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              }
+            })
+              .then((response) => response.json())
+              .then((data) => console.log(data))
+              .catch((error) => console.error('Error:', error));
+          }
         });
+        // update column index
+        colIndex += 1;
       });
 
       table.appendChild(tr);
+      // update row index
+      rowIndex += 1;
     });
   });
