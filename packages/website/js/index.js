83894f350098f31bc63dd943f482c0b427db2a52
Nicu Savin <savinicu@gmail.com>
2023-08-03 14:01:20 +0200

    added palette and color change functionality

--- packages/website/js/index.js
+++ packages/website/js/index.js
@@ -10,6 +10,7 @@ fetch('http://localhost:3003/canvas')
   .then((res) => res.json())
   .then((res) => {
     let table = document.getElementById('gridTable');
+    let palette = document.getElementById('palette');
     // each element in the res will be assigned a row
     res.forEach((row) => {
       let tr = document.createElement('tr');
@@ -19,6 +20,10 @@ fetch('http://localhost:3003/canvas')
         let td = document.createElement('td');
         td.style.backgroundColor = cell;
         tr.appendChild(td);
+        td.addEventListener('click', function () {
+          // selecting the grid to be colored
+          this.style.backgroundColor = palette.value;
+        });
       });
 
       table.appendChild(tr);
